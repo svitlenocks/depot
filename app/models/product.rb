b5bc5a7a2e399d3d4aa1e0b66e11053e7d0878bb
Vlad Kamysh <svitlenocks23@gmail.com>
2020-03-09 23:56:05 +0300

    Add cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,19 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true, length: { minimum: 10 }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :image_url, allow_blank: true, format: { with: /\.(gif|jpg|png)\Z/i }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    return true if line_items.empty?
+
+    errors.add(:base, "line items exist")
+    false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root to: 'store#index', as: 'store'
 
   get 'store/index'
+
   resources :products
+  resources :line_items
+  resources :carts
 end

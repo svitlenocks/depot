f76a45190f4a70f823d276590562b4dc31ce9db8
Vlad Kamysh <svitlenocks23@gmail.com>
2020-03-15 18:00:54 +0300

    Add form for new order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  PAYMENT_TYPES = ["Check", "Credit card", "Purchase order"].freeze
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
